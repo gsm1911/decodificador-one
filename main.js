38e7dca70ebbe47d5434be3649cde14ec46f84a6
Gustavo Maeda <gustavosm115@outlook.com>
2024-09-09 18:42:47 -0300

    Otimização da função criptografar()

--- main.js
+++ main.js
@@ -2,30 +2,14 @@ var pat = /^[a-z\s]+$/;
 
 function criptografar(){
     let entrada = String(document.getElementById('entrada_texto').value);
-    let saida = [];
-    if(pat.test(entrada) == true){
-        for(let i = 0; i<entrada.length; i++){
-            if(entrada[i] == 'e'){
-                saida[i] = 'enter';
-            }
-            else if(entrada[i] == 'i'){
-                saida[i] = 'imes';
-            }
-            else if(entrada[i] == 'a'){
-                saida[i] = 'ai';
-            }
-            else if(entrada[i] == 'o'){
-                saida[i] = 'ober';
-            }
-            else if(entrada[i] == 'u'){
-                saida[i] = 'ufat';
-            }
-            else{
-                saida[i] = entrada[i];
-            }
-        }
     
-        document.getElementById("saida_texto").value = saida.join("");
+    if(pat.test(entrada) == true){
+        entrada = entrada.replaceAll("e", "enter");
+        entrada = entrada.replaceAll("i", "imes");
+        entrada = entrada.replaceAll("a", "ai");
+        entrada = entrada.replaceAll("o", "ober");
+        entrada = entrada.replaceAll("u", "ufat");
+        document.getElementById("saida_texto").value = entrada
     }
     else{
         document.getElementById("saida_texto").value = "Insira somente letras minusculas e sem acento."
